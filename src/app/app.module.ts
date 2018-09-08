5c83467fb434eb2b4b6f60e70a758b0172378be1
Matt Anderson <him@backattheranch.ca>
2018-09-08 16:00:17 -0700

    Add CreateProject, VersionNames

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -9,10 +10,13 @@ import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NamesService } from './services/names.service';
 import { AuthService } from './services/auth.service';
+import { ProjectsService } from './services/projects.service';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { RegisterComponent } from './register/register.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { UtilityNavigationComponent } from './utility-navigation/utility-navigation.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
+import { VersionNamesComponent } from './version-names/version-names.component';
 
 const ROUTES = [
   {
@@ -36,6 +40,14 @@ const ROUTES = [
   {
     path: 'projects',
     component: ProjectsComponent
+  },
+  {
+    path: 'create-project',
+    component: CreateProjectComponent
+  },
+  {
+    path: 'version-names/:id',
+    component: VersionNamesComponent
   }
 ];
 
@@ -47,15 +59,19 @@ const ROUTES = [
     SignInComponent,
     RegisterComponent,
     ProjectsComponent,
-    UtilityNavigationComponent
+    UtilityNavigationComponent,
+    CreateProjectComponent,
+    VersionNamesComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [NamesService, AuthService],
+  providers: [NamesService, AuthService, ProjectsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
