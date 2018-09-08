b193869f447da3b2333eee8b05085d07c8ae0e6d
Matt Anderson <him@backattheranch.ca>
2018-09-08 16:08:42 -0700

    Complete register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { PasswordValidation } from './password-validation';
+import { RegisterResponse } from './../models/register-response.model';
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +12,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  userForm: FormGroup;
+  password: AbstractControl;
+  strongRegularExp: RegExp;
+  mediumRegularExp: RegExp;
+  private registrationState: null | string;
+  private successValue = 'success';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router, public authService: AuthService) {
+    this.strongRegularExp = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})');
+    this.mediumRegularExp = new RegExp('^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})')
+    this.createForm();
+    this.password = this.userForm.controls['password'];
+    this.registrationState = 'prereg';
+  }
 
   ngOnInit() {
+  }
+
+  goToSignIn() {
+    this.registrationState = null;
+    this.router.navigate(['./', { outlets: { signin: ['sign-in']}}]);
+  }
+
+  createForm() {
+    this.userForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: PasswordValidation.MatchPassword
+    });
+  }
+
+  register(form) {console.log('#### register CALLED');
+    this.authService.register(form.value)
+      .subscribe((res) => {
+        if (res.code === this.successValue) {
+          this.registrationState = this.successValue;
+        }
+      });
+  }
+
+  passwordStrength() {
+    if (this.strongRegularExp.test(this.password.value)) {
+      return 'strong';
+    }
+    if (this.mediumRegularExp.test(this.password.value)) {
+      return 'medium';
+    }
+    return 'weak';
+  }
+
+  isWeak() {
+    return this.password.dirty && this.passwordStrength() === 'weak';
+  }
+  
+  isMedium() {
+    return this.password.dirty && this.passwordStrength() === 'medium';
+  }
+  
+  isStrong() {
+    return this.password.dirty && this.passwordStrength() === 'strong';
+  }
+
+  passwordMatchError() {
+    const confirmPassword = this.userForm.controls.confirmPassword;
+
+    return confirmPassword.errors && confirmPassword.errors.MatchPassword && confirmPassword.dirty;
   }
 
 }
