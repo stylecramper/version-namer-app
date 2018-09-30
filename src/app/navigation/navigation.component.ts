0ad7e7b1e504fc9ba665cbf95b983431e34e8560
Matt Anderson <him@backattheranch.ca>
2018-09-30 13:47:12 -0700

    Move nav highlight to Home link on logout

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './../services/auth.service';
+
 @Component({
   selector: 'navigation',
   templateUrl: './navigation.component.html',
@@ -8,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
   activeLink: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    this.authService.getLoggedInStatus
+      .subscribe(status => this.changeLoggedInStatus(status));
+  }
 
   ngOnInit() {
     this.activeLink = 'home';
+  }
+
+  changeLoggedInStatus(status: boolean): void {
+    if (!status) {
+      this.activeLink = 'home';
+    }
   }
 
 }
