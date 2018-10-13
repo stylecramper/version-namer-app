d96efded7cd954567e352f7ed109521a3a6e99b7
Matt Anderson <him@backattheranch.ca>
2018-10-13 10:46:21 -0700

    Use Angular Material button for "Create Project" button

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AuthService } from '../services/auth.service';
 import { ProjectsService } from './../services/projects.service';
